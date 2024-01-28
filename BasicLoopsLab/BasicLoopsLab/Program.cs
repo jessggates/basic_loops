7d4da75689e92e137ec0d700073b86725ba90022
Jessica Gates <gatespj@outlook.com>
2024-01-28 13:29:47 -0500

    second question complete

--- BasicLoopsLab/BasicLoopsLab/Program.cs
+++ BasicLoopsLab/BasicLoopsLab/Program.cs
@@ -5,8 +5,40 @@ do
 {
     Console.WriteLine("Hello, World!");
     Console.WriteLine("Would you like to continue (y/n)");
-    userInput = Console.ReadLine();
+    userInput = Console.ReadLine().ToLower().Trim();
 }
 while (userInput == "y");
 Console.WriteLine("Goodbye!");
+Console.WriteLine();
+
+
+int userNumber = 0;
+string keepPlaying = "";
+
+do
+{
+    Console.Write("Enter a number: ");
+    userNumber = int.Parse(Console.ReadLine());
+
+    for (int i = userNumber; i >= 0; i--)
+    {
+        Console.Write(i + " ");
+    }
+    {
+        Console.WriteLine();
+    }
+    for (int i = 0; i <= userNumber; i ++)
+    {
+        Console.Write(i + " ");
+    }
+    {
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("Would you like to continue? (y/n)");
+    keepPlaying = Console.ReadLine().ToLower().Trim();
+}
+while (keepPlaying == "y");
+Console.WriteLine("Goodbye!");
+Console.WriteLine();
 
